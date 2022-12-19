7014dfdee7da568c3d77d47c6ed3f08b7e910573
Wyatt <wyatt.osterling@wizardwatch.net>
2022-12-19 07:20:59 -0500

    as;dlkfj;lkj

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -42,7 +42,8 @@ in
       system.activationScripts.minecraft-server-data-dir.text = ''
           mkdir -p ${cfg.dataDir}
           touch ${cfg.dataDir}/start.sh
-          echo "wget -nc -q -O "/srv/minecraft/server.jar" "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar"
+          echo "#!/bin/sh
+wget -nc -q -O "/srv/minecraft/server.jar" "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar"
 wget -nc -q -O "/srv/minecraft/packwiz-installer-bootstrap.jar" "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar"
 java -jar /srv/minecraft/packwiz-installer-boostrap.jar
 java -Xmx6G -jar /srv/minecraft/fabric-server-launch.jar nogui" >> ${cfg.dataDir}/start.sh
