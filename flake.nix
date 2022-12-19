a7828d6b5963edc4387641380cf1763c43c67c6e
Wyatt <wyatt.osterling@wizardwatch.net>
2022-12-19 06:11:31 -0500

    Hope and plea

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   description = "Trix, short for tree nix, supplies various useful functions.";
   outputs = { self, nixpkgs, ...}: rec {
-    nixosModules.default = {
-      minecraft = import ./modules/minecraft.nix;
-    };
+    nixosModules.default = import ./modules/minecraft.nix;
+    
   };
 }
