ac46e0b7cacbd74da1f437bf9b697326278fa59e
Wyatt <wyatt.osterling@wizardwatch.net>
2022-12-19 18:10:41 -0500

    hopefully

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -46,8 +46,8 @@ in
           echo "#!/bin/sh
 wget -nc -q -O "/srv/minecraft/server.jar" "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar"
 wget -nc -q -O "/srv/minecraft/packwiz-installer-bootstrap.jar" "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar"
-java -jar /srv/minecraft/packwiz-installer-boostrap.jar
-java -Xmx6G -jar /srv/minecraft/fabric-server-launch.jar nogui" >> ${cfg.dataDir}/start.sh
+${jre_headless}/bin/java -jar /srv/minecraft/packwiz-installer-boostrap.jar
+${jre_headless}/bin/java -Xmx6G -jar /srv/minecraft/fabric-server-launch.jar nogui" >> ${cfg.dataDir}/start.sh
           chown minecraft:minecraft ${cfg.dataDir}
           chmod -R 775 ${cfg.dataDir}
       '';
