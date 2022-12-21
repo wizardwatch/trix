b406ad4f25e283b35eb05ddf4e66679b43fda946
Wyatt <wyatt.osterling@wizardwatch.net>
2022-12-20 21:37:25 -0500

    Almost works!

--- flake.nix
+++ flake.nix
@@ -1,19 +1,29 @@
 {
   description = "Trix, short for tree nix, supplies various useful functions.";
+  #inputs.sops-nix.url = github:Mic92/sops-nix;
   outputs = { self, nixpkgs, ...}: rec {
     nixosModules.default = import ./modules/minecraft.nix;
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ({ pkgs, ...}: {
-        boot.isContainer = true;
+        #boot.isContainer = true;
         system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-        networking.useDHCP = false;
-        privateNetwork = true;
-        hostAddress = "192.168.100.2";
-        localAddress = "192.168.100.11";
+        networking.useDHCP = true;
+        #privateNetwork = true;
+        #hostAddress = "192.168.100.2";
+        #localAddress = "192.168.100.11";
       })
       nixosModules.default
       {trix.services.minecraft.enable = true;}
+      {
+      users.users.test = {
+        isNormalUser = true;
+        extraGroups = ["wheel"];
+        password = "1234";
+      };
+    }
+        #sops-nix.nixosModules.sops
+
       ];
     };
   };
--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -33,7 +33,7 @@ in
         serviceConfig = {
         Type = "simple";
         User = "minecraft";
-        RestartSec = "10s";
+        RestartSec = "60s";
         Restart = "always";
         StandardError= "journal";
         path = [pkgs.bash pkgs.openjdk17 pkgs.wget pkgs.sha1collisiondetection];
@@ -50,12 +50,12 @@ in
           touch ${cfg.dataDir}/start.sh
           echo "#!/bin/sh
 echo "${sha1}"
-${wget}/bin/wget -nc -O "/srv/minecraft/server.jar" "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar"
+${wget}/bin/wget -nc -O "/srv/minecraft/server_quilt.jar" "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/latest/quilt-installer-latest.jar"
 ${wget}/bin/wget -nc -O "/srv/minecraft/packwiz-installer-bootstrap.jar" "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar"
 chmod +x /srv/minecraft/packwiz-installer-bootstrap.jar
 chmod +x /srv/minecraft/fabric-server-launch.jar 
-${jre_headless}/bin/java -jar /srv/minecraft/packwiz-installer-bootstrap.jar
-${jre_headless}/bin/java -Xmx6G -jar /srv/minecraft/fabric-server-launch.jar nogui" >> ${cfg.dataDir}/start.sh
+${jre_headless}/bin/java -jar /srv/minecraft/packwiz-installer-bootstrap.jar -g -s server https://ryleu.me/winter-wonderland-pack/main/pack.toml
+${jre_headless}/bin/java -Xmx4096m -jar server_quilt.jar nogui" >> ${cfg.dataDir}/start.sh
 chmod +x ${cfg.dataDir}
           chown minecraft:minecraft ${cfg.dataDir}
           chmod -R 775 ${cfg.dataDir}
